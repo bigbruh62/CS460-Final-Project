e213a522ae8b2dcf0b6bb9a0911d4374170bdf8c
bigbruh62 <nicolasw.palmer@gmail.com>
2018-04-09 12:10:47 -0600

    Add files via upload

---
+++ fortuneWebClient/main.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include "client.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QApplication::setApplicationDisplayName(Client::tr("Fortune Client"));
+    Client client;
+    client.show();
+
+    return app.exec();
+}
---
+++ fortuneWebServer/server.cpp
@@ -0,0 +1,139 @@
+#include <QtWidgets>
+#include <QtNetwork>
+#include <QtCore>
+
+#include "server.h"
+
+Server::Server(QWidget *parent) : QDialog(parent), statusLabel(new QLabel)
+{
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    statusLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
+
+    QNetworkConfigurationManager manager;
+    if (manager.capabilities() & QNetworkConfigurationManager::NetworkSessionRequired)
+    {
+        QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+        settings.beginGroup(QLatin1String("QtNetwork"));
+        const QString id = settings.value(QLatin1String("DefaultNetworkConfiguration")).toString();
+        settings.endGroup();
+
+        QNetworkConfiguration config = manager.configurationFromIdentifier(id);
+        if ((config.state() & QNetworkConfiguration::Discovered) != QNetworkConfiguration::Discovered)
+        {
+            config = manager.defaultConfiguration();
+        }
+
+        networkSession = new QNetworkSession(config, this);
+        connect(networkSession, &QNetworkSession::opened, this, &Server::sessionOpened);
+
+        statusLabel->setText(tr("Opening network session."));
+        networkSession->open();
+    }
+    else
+    {
+        sessionOpened();
+    }
+
+    fortunes << tr("You've been leading a dog's life. Stay off the furniture.")
+                 << tr("You've got to think about tomorrow.")
+                 << tr("You will be surprised by a loud noise.")
+                 << tr("You will feel hungry again in another hour.")
+                 << tr("You might have mail.")
+                 << tr("You cannot kill time without injuring eternity.")
+                 << tr("Computers are not intelligent. They only think they are.")
+                 << tr("Study hard and you may pass the class.")
+                 << tr("Brother Jones, you should give us all A's");
+
+    auto quitButton = new QPushButton(tr("Quit"));
+    quitButton->setAutoDefault(false);
+    connect(quitButton, &QAbstractButton::clicked, this, &QWidget::close);
+    connect(tcpServer, &QTcpServer::newConnection, this, &Server::sendFortune);
+
+    auto buttonLayout = new QHBoxLayout;
+    buttonLayout->addStretch(1);
+    buttonLayout->addWidget(quitButton);
+    buttonLayout->addStretch(1);
+
+    QVBoxLayout *mainLayout = nullptr;
+    if (QGuiApplication::styleHints()->showIsFullScreen() || QGuiApplication::styleHints()->showIsMaximized())
+    {
+        auto outerVerticalLayout = new QVBoxLayout(this);
+        outerVerticalLayout->addItem(new QSpacerItem(0,0,QSizePolicy::Ignored, QSizePolicy::MinimumExpanding));
+
+        auto outerHorizontalLayout = new QHBoxLayout;
+        outerHorizontalLayout->addItem(new QSpacerItem(0,0,QSizePolicy::MinimumExpanding, QSizePolicy::Ignored));
+
+        auto groupBox = new QGroupBox(QGuiApplication::applicationDisplayName());
+        mainLayout = new QVBoxLayout(groupBox);
+        outerHorizontalLayout->addWidget(groupBox);
+        outerHorizontalLayout->addItem(new QSpacerItem(0,9,QSizePolicy::MinimumExpanding, QSizePolicy::Ignored));
+        outerVerticalLayout->addLayout(outerHorizontalLayout);
+        outerVerticalLayout->addItem(new QSpacerItem(0,0,QSizePolicy::Ignored, QSizePolicy::MinimumExpanding));
+    }
+    else
+    {
+        mainLayout = new QVBoxLayout(this);
+    }
+
+    mainLayout->addWidget(statusLabel);
+    mainLayout->addLayout(buttonLayout);
+
+    setWindowTitle(QGuiApplication::applicationDisplayName());
+}
+
+void Server::sessionOpened()
+{
+    if (networkSession)
+    {
+        QNetworkConfiguration config = networkSession->configuration();
+        QString id;
+        if (config.type() == QNetworkConfiguration::UserChoice)
+            id = networkSession->sessionProperty(QLatin1String("UserChoiceConfiguration")).toString();
+        else
+            id = config.identifier();
+
+        QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+        settings.beginGroup(QLatin1String("QtNetwork"));
+        settings.setValue(QLatin1String("DefaultNetworkConfiguration"), id);
+        settings.endGroup();
+    }
+
+    tcpServer = new QTcpServer(this);
+    if (!tcpServer->listen())
+    {
+        QMessageBox::critical(this, tr("Fortune Server"), tr("Unable to start the server: %1.").arg(tcpServer->errorString()));
+        close();
+        return;
+    }
+    QString ipAddress;
+    QList<QHostAddress> ipAddressesList = QNetworkInterface::allAddresses();
+    for (int i = 0; i < ipAddressesList.size(); i++)
+    {
+        if (ipAddressesList.at(i) != QHostAddress::LocalHost && ipAddressesList.at(i).toIPv4Address())
+        {
+            ipAddress = ipAddressesList.at(i).toString();
+            break;
+        }
+    }
+    if (ipAddress.isEmpty())
+    {
+        ipAddress = QHostAddress(QHostAddress::LocalHost).toString();
+
+    }
+         statusLabel->setText(tr("The server is running on \n\nIP: %1\nport: %2\n\n" "run the Fortune Client now.").arg(ipAddress).arg(tcpServer->serverPort()));
+}
+
+void Server::sendFortune()
+{
+    QByteArray block;
+    QDataStream out(&block, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_5_10);
+
+    out << fortunes[QRandomGenerator::global()->bounded(fortunes.size())];
+
+    QTcpSocket *clientConnection = tcpServer->nextPendingConnection();
+    connect(clientConnection, &QAbstractSocket::disconnected, clientConnection, &QObject::deleteLater);
+
+    clientConnection->write(block);
+    clientConnection->disconnectFromHost();
+}
